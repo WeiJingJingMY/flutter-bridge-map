a49ca4e33515afbfdf944d5e07857d9a49c74ec8
懒洋洋 <www.848258323@live.com>
2026-08-17 15:44:41 +0800

    Add pages/main.tsx

---
+++ pages/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { GuideSite } from "../app/GuideSite";
+import "../app/globals.css";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <GuideSite />
+  </React.StrictMode>,
+);
+
